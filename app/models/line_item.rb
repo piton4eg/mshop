4d70692b70bf2d6f97e3cb6dd55e7645df8c0e51
Markov Alexey <piton4eg@mail.ru>
2014-02-14 00:17:38 +0400

    Some fix

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -3,8 +3,6 @@ class LineItem < ActiveRecord::Base
   belongs_to :product
   belongs_to :cart
 
-  validates :order_id, :product_id, :cart_id, :quantity, presence: true
-
   def total_price
     product.price * quantity
   end
