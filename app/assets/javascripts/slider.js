2c412fd550f7bae0908252828aa6de960e838a65
axi.traene <axi.traene@gmail.com>
2014-02-20 17:00:20 +0400

    некоторые изменения на главной

--- app/assets/javascripts/slider.js
+++ app/assets/javascripts/slider.js
@@ -2,14 +2,14 @@ $(document).ready(function(){
 	$(function() {
 		$('#slides').slidesjs({
 			width: 940,
-			height: 445,
-			play: {
-				active: true,
-				auto: true,
-				interval: 6000,
-				pauseOnHover: true,
-				swap: true
-			}
+			height: 445
+//			play: {
+//				active: true,
+//				auto: true,
+//				interval: 6000,
+//				pauseOnHover: true,
+//				swap: true
+//			}
 		});
 	});
 });
