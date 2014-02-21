530dc619ca5281cb2a786d7e88258516ba924120
axi.traene <axi.traene@gmail.com>
2014-02-21 15:26:08 +0400

    подрагивание текста + скрипт немного

--- app/assets/javascripts/totop.js
+++ app/assets/javascripts/totop.js
@@ -8,10 +8,6 @@ $(function() {
 		if ($(window).scrollTop() <= winHeight) $(scrollDiv).fadeOut("slow")
 		else $(scrollDiv).fadeIn("slow")
 	});
-	$(window).scroll(function() {
-		if ($(window).scrollTop() >= go_down.height()-"999") $(scrollDiv_2).fadeOut("slow")
-		else $(scrollDiv_2).fadeIn("slow")
-	});
 	$("#topjs").click(function() {
 		$("html, body").animate({scrollTop: 0}, "slow")
 	})
